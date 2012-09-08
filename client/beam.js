67abb2fd2a3b48861ed86ff8e872e11be82423f8
Lars Ingebrigtsen <larsi@gnus.org>
2012-09-08 20:16:33 +0200

    Change indentation to two spaces, as recommended by http://atroche.org/post/30994290348/javascript-indentation

--- client/beam.js
+++ client/beam.js
@@ -1,182 +1,181 @@
 var prefixTable;
 
 function decorateHeddle() {
-    var results = document.getElementById("searchResults");
-    var input = document.getElementById("search");
+  var results = document.getElementById("searchResults");
+  var input = document.getElementById("search");
 
-    input.onkeyup = function() {
-	displayResults(input, results);
-    };
+  input.onkeyup = function() {
+    displayResults(input, results);
+  };
 
-    prefixTable = new Array();
-    map(groups, function(group) {
-	map(group.split("."), function(bit) {
-	    if (bit.length > 2)
-		for (var i = 2; i < bit.length + 1; i++) {
-		    var sub = "pr." + bit.substring(0, i);
-		    if (! prefixTable[sub])
-			prefixTable[sub] = new Array(group);
-		    else
-			prefixTable[sub].push(group);
-		}
-	});
+  prefixTable = new Array();
+  map(groups, function(group) {
+    map(group.split("."), function(bit) {
+      if (bit.length > 2)
+	for (var i = 2; i < bit.length + 1; i++) {
+	  var sub = "pr." + bit.substring(0, i);
+	  if (! prefixTable[sub])
+	    prefixTable[sub] = new Array(group);
+	  else
+	    prefixTable[sub].push(group);
+	}
     });
+  });
 
-    addInfiniteScroll();
-    checkPageSize();
+  addInfiniteScroll();
+  checkPageSize();
 }
 
 function addInfiniteScroll() {
-    // setup infinite scroll
-    infiniteScroll({
-	distance: 50,
-	callback: extendGroupPage
-    });
+  // setup infinite scroll
+  infiniteScroll({
+    distance: 50,
+    callback: extendGroupPage
+  });
 }
 
 function extendGroupPage(done) {
-    var roots = document.getElementById("roots");
-    var page = parseInt(roots.getAttribute("page")) + 1;
-    var group = roots.getAttribute("group");
-    if (! group)
-	return;
-    getHTML("http://localhost:8080/group/" + group + "/" +
-	    page + "/naked",
-	    function(html) {
-		var div = document.createElement("div");
-		div.innerHTML = html;
-		decorateGroupLinks(div);
-		map(div.childNodes,
-		    function(node) {
-			roots.appendChild(node);
-		    });
-		roots.setAttribute("page", page);
-		if (done)
-		    done();
-		checkPageSize();
-	    });
+  var roots = document.getElementById("roots");
+  var page = parseInt(roots.getAttribute("page")) + 1;
+  var group = roots.getAttribute("group");
+  if (! group)
+    return;
+  getHTML("http://localhost:8080/group/" + group + "/" +
+	  page + "/naked",
+	  function(html) {
+	    var div = document.createElement("div");
+	    div.innerHTML = html;
+	    decorateGroupLinks(div);
+	    map(div.childNodes,
+		function(node) {
+		  roots.appendChild(node);
+		});
+	    roots.setAttribute("page", page);
+	    if (done)
+	      done();
+	    checkPageSize();
+	  });
 }
 
 function checkPageSize() {
-    if (documentHeight() - 50 < windowHeight())
-	extendGroupPage();
+  if (documentHeight() - 50 < windowHeight())
+    extendGroupPage();
 }
 
 function displayResults(input, results) {
-    var matches = prefixTable["pr." + input.value];
-    if (! matches)
-	return;
-    removeChildren(results);
-    map(matches, function(group) {
-	group = "gwene." + group;
-	var a = document.createElement("a");
-	a.href = "/group/" + group;
-	a.onclick = function() {
-	    displayGroup(group);
-	    return false;
-	}
-	a.innerHTML = group + "<br>";
-	results.appendChild(a);
-    });
+  var matches = prefixTable["pr." + input.value];
+  if (! matches)
+    return;
+  removeChildren(results);
+  map(matches, function(group) {
+    group = "gwene." + group;
+    var a = document.createElement("a");
+    a.href = "/group/" + group;
+    a.onclick = function() {
+      displayGroup(group);
+      return false;
+    }
+    a.innerHTML = group + "<br>";
+    results.appendChild(a);
+  });
 }
 
 function displayGroup(group) {
-    getHTML("http://localhost:8080/group/" + group + "/0/naked",
-	    function(html) {
-		var roots = document.getElementById("roots");
-		roots.innerHTML = html;
-		decorateGroupLinks(roots);
-		roots.setAttribute("group", group);
-		roots.setAttribute("page", 0);
-		checkPageSize();
-	    });
+  getHTML("http://localhost:8080/group/" + group + "/0/naked",
+	  function(html) {
+	    var roots = document.getElementById("roots");
+	    roots.innerHTML = html;
+	    decorateGroupLinks(roots);
+	    roots.setAttribute("group", group);
+	    roots.setAttribute("page", 0);
+	    checkPageSize();
+	  });
 }
 
 function decorateGroup(group) {
-    var roots = document.getElementById("roots");
-    decorateGroupLinks(roots);
-    roots.setAttribute("page", 0);
-    roots.setAttribute("group", group);
-    addInfiniteScroll();
-    checkPageSize();
+  var roots = document.getElementById("roots");
+  decorateGroupLinks(roots);
+  roots.setAttribute("page", 0);
+  roots.setAttribute("group", group);
+  addInfiniteScroll();
+  checkPageSize();
 }
 
 function decorateGroupLinks(roots) {
-    map(roots.getElementsByTagName("A"),
-	function(link) {
-	    if (link.onclick)
-		return;
-	    link.onclick = function() {
-		var old = document.getElementById(articleId(link.href));
-		if (old)
-		    removeElem(old);
-		else 
-		    insertThread(link);
-		return false;
-	    };
-	});
+  map(roots.getElementsByTagName("A"),
+      function(link) {
+	if (link.onclick)
+	  return;
+	link.onclick = function() {
+	  var old = document.getElementById(articleId(link.href));
+	  if (old)
+	    removeElem(old);
+	  else
+	    insertThread(link);
+	  return false;
+	};
+      });
 }
 
 function insertThread(link) {
-    getHTML(link.href + "/naked",
-	    function(html) {
-		var div = document.createElement("div");
-		div.className = "thread";
-		div.innerHTML = html;
-		var first = true;
-		map(div.getElementsByTagName("A"),
-		    function(link) {
-			if (first && link.href.match(/^http/)) {
-			    first = false;
-			    addThumbnail(div, link.href);
-			}
-			link.onclick = function() {
-			    window.open(link.href, '_blank');
-			    window.focus();
-			    return false;
-			};
-		    });
-		addPermalink(div, link.href);
-		div.id = articleId(link.href);
-		var next = link.nextSibling;
-		if (next.className == "comments")
-		    next = next.nextSibling;
-		link.parentNode.insertBefore(div, next);
-	    });
+  getHTML(link.href + "/naked",
+	  function(html) {
+	    var div = document.createElement("div");
+	    div.className = "thread";
+	    div.innerHTML = html;
+	    var first = true;
+	    map(div.getElementsByTagName("A"),
+		function(link) {
+		  if (first && link.href.match(/^http/)) {
+		    first = false;
+		    addThumbnail(div, link.href);
+		  }
+		  link.onclick = function() {
+		    window.open(link.href, '_blank');
+		    window.focus();
+		    return false;
+		  };
+		});
+	    addPermalink(div, link.href);
+	    div.id = articleId(link.href);
+	    var next = link.nextSibling;
+	    if (next.className == "comments")
+	      next = next.nextSibling;
+	    link.parentNode.insertBefore(div, next);
+	  });
 }
 
 function addPermalink(div, url) {
-    var link = document.createElement("a");
-    link.href = url;
-    link.innerHTML = "&nbsp;☉";
-    link.className = "permalink";
-    link.title = "Permalink";
-    var inner = getChildOfType(div, "DIV");
-    inner.insertBefore(link, inner.childNodes[0]);
+  var link = document.createElement("a");
+  link.href = url;
+  link.innerHTML = "&nbsp;☉";
+  link.className = "permalink";
+  link.title = "Permalink";
+  var inner = getChildOfType(div, "DIV");
+  inner.insertBefore(link, inner.childNodes[0]);
 }
 
 function addThumbnail(div, url) {
-    var image = new Image();
-    image.src = "http://immediatenet.com/t/m?Size=1024x768&URL=" + url;
-    image.onload = function() {
-	var link = document.createElement("a");
-	link.innerHTML =
-	    "<img src=\"http://immediatenet.com/t/m?Size=1024x768&URL=" +
-	    url + "\">";
-	link.href = url;
-	link.className = "thumbnail";
-	div.insertBefore(link, div.childNodes[0]);
-	link.onclick = function() {
-	    window.open(link.href, '_blank');
-	    window.focus();
-	    return false;
-	};
+  var image = new Image();
+  image.src = "http://immediatenet.com/t/m?Size=1024x768&URL=" + url;
+  image.onload = function() {
+    var link = document.createElement("a");
+    link.innerHTML =
+      "<img src=\"http://immediatenet.com/t/m?Size=1024x768&URL=" +
+      url + "\">";
+    link.href = url;
+    link.className = "thumbnail";
+    div.insertBefore(link, div.childNodes[0]);
+    link.onclick = function() {
+      window.open(link.href, '_blank');
+      window.focus();
+      return false;
     };
+  };
 }
 
 function articleId(url) {
-    var regs = url.match(/[0-9]+$/);
-    if (regs)
-	return "article" + regs;
+  var regs = url.match(/[0-9]+$/);
+  if (regs)
+    return "article" + regs;
 }
-
--- server/heddle.js
+++ server/heddle.js
@@ -13,284 +13,284 @@ Buffer = require('buffer').Buffer,
 exec = require('child_process').exec;
 
 function issue404(response) {
-    response.writeHeader(404, {"Content-Type": "text/plain"});
-    response.write("404 Not Found\n");
-    response.end();
+  response.writeHeader(404, {"Content-Type": "text/plain"});
+  response.write("404 Not Found\n");
+  response.end();
 }
 
 http.createServer(function(request, response) {
-    var file = url.parse(request.url).pathname;
+  var file = url.parse(request.url).pathname;
 
-    util.puts(file);
+  util.puts(file);
 
-    if (file == "/")
-	file = "/client/index.html";
-    
-    if (file.match(/^.client/))
-	outputStatic(file, response);
-    else if (file.match(/^.group/))
-	outputGroup(file, response);
-    else if (file.match(/^.thread/))
-	outputThread(file, response);
-    else
-	issue404(response);
+  if (file == "/")
+    file = "/client/index.html";
+
+  if (file.match(/^.client/))
+    outputStatic(file, response);
+  else if (file.match(/^.group/))
+    outputGroup(file, response);
+  else if (file.match(/^.thread/))
+    outputThread(file, response);
+  else
+    issue404(response);
 }).listen(8080);
 
 function outputStatic(file, response) {
-    if (file.match(/\.\./)) {
-	issue404();
-	return;
-    }
-    
-    var full_path = path.join(clientPath, file);
+  if (file.match(/\.\./)) {
+    issue404();
+    return;
+  }
 
-    path.exists(full_path, function(exists) {
-        if (! exists)
-	    issue404(response);
-        else {
-	  fs.readFile(full_path, "binary", function(err, file) {
-	      if (err) {
-		  response.writeHeader(500, {"Content-Type": "text/plain"});
-		  response.write(err + "\n");
-		  response.end();
-	      } else {
-		  if (full_path.match(/\.html$/))
-		      contentType = "text/html";
-		  else if (full_path.match(/\.css$/))
-		      contentType = "text/css";
-		  else if (full_path.match(/\.png$/))
-		      contentType = "image/png";
-		  else
-		      contentType = "text/plain";
-		  response.writeHeader(200, {"Content-Type": contentType +
-					     " ;charset=utf-8"});
-		  response.write(file, "binary");
-		  response.end();
-	      }
-            });
-        }
+  var full_path = path.join(clientPath, file);
+
+  path.exists(full_path, function(exists) {
+    if (! exists)
+      issue404(response);
+    else {
+      fs.readFile(full_path, "binary", function(err, file) {
+	if (err) {
+	  response.writeHeader(500, {"Content-Type": "text/plain"});
+	  response.write(err + "\n");
+	  response.end();
+	} else {
+	  if (full_path.match(/\.html$/))
+	    contentType = "text/html";
+	  else if (full_path.match(/\.css$/))
+	    contentType = "text/css";
+	  else if (full_path.match(/\.png$/))
+	    contentType = "image/png";
+	  else
+	    contentType = "text/plain";
+	  response.writeHeader(200, {"Content-Type": contentType +
+				     " ;charset=utf-8"});
+	  response.write(file, "binary");
+	  response.end();
+	}
       });
+    }
+  });
 }
 
 function outputGroup(url, response) {
-    var regs = url.match(/\/group\/([^\/]+)(\/([0-9]+)(\/naked)?)?/);
-    if (! regs)
+  var regs = url.match(/\/group\/([^\/]+)(\/([0-9]+)(\/naked)?)?/);
+  if (! regs)
+    return;
+  var group = regs[1];
+  var page = regs[3];
+  var naked = regs[4];
+  if (! page)
+    page = 0;
+  else
+    page = parseInt(page);
+
+  var warp = path.normalize(root + "/" + group.replace(/\./g, "/") + "/WARP");
+
+  path.exists(warp, function(exists) {
+    if (! exists) {
+      issue404(response);
+      return;
+    }
+
+    fs.open(warp, "r", function(err, fd) {
+      if (err) {
+	util.puts(err);
 	return;
-    var group = regs[1];
-    var page = regs[3];
-    var naked = regs[4];
-    if (! page)
-	page = 0;
-    else
-	page = parseInt(page);
+      }
 
-    var warp = path.normalize(root + "/" + group.replace(/\./g, "/") + "/WARP");
+      // Find the start of each segment.
+      var buffer = new Buffer(8);
+      fs.read(fd, buffer, 0, 8, 0, function(err, bytesRead) {
+	var numberOfRoots = buffer.readUInt32LE(0)
+	var lastArticle = buffer.readUInt32LE(4)
 
-    path.exists(warp, function(exists) {
-        if (! exists) {
-	    issue404(response);
-	    return;
+	if (page + 2 > numberOfRoots / 30) {
+	  issue404(response);
+	  return;
 	}
 
-	fs.open(warp, "r", function(err, fd) {
-	    if (err) {
-		util.puts(err);
-		return;
-	    }
-
-	    // Find the start of each segment.
-	    var buffer = new Buffer(8);
-   	    fs.read(fd, buffer, 0, 8, 0, function(err, bytesRead) {
-		var numberOfRoots = buffer.readUInt32LE(0)
-		var lastArticle = buffer.readUInt32LE(4)
-
-		if (page + 2 > numberOfRoots / 30) {
-		    issue404(response);
-		    return;
-		}
-
-		// Find the start of the root segment.
-   		fs.read(fd, buffer, 0, 8, 4 * (2 + lastArticle + page),
-			function(err, bytesRead) {
-			    var pageStart = buffer.readUInt32LE(0);
-			    var pageEnd = buffer.readUInt32LE(4);
-			    buffer = new Buffer(pageEnd - pageStart);
-   			    fs.read(fd, buffer, 0, pageEnd - pageStart,
-				    pageStart, function(err, bytesRead) {
-					writeRoots(response, buffer,
-   						   group, naked);
-				    });
-			});
-	    });
-	});
-	
+	// Find the start of the root segment.
+   	fs.read(fd, buffer, 0, 8, 4 * (2 + lastArticle + page),
+		function(err, bytesRead) {
+		  var pageStart = buffer.readUInt32LE(0);
+		  var pageEnd = buffer.readUInt32LE(4);
+		  buffer = new Buffer(pageEnd - pageStart);
+   		  fs.read(fd, buffer, 0, pageEnd - pageStart,
+			  pageStart, function(err, bytesRead) {
+			    writeRoots(response, buffer,
+   				       group, naked);
+			  });
+		});
+      });
     });
+
+  });
 }
 
 function writeRoots(response, buffer, group, naked) {
-    var i = 0;
-    var length = buffer.length;
-    var char;
-    var from;
-    
-    response.writeHeader(200, {"Content-Type":
-			       "text/html; charset=utf-8"});
+  var i = 0;
+  var length = buffer.length;
+  var char;
+  var from;
 
-    if (! naked)
-	writeFile(path.join(clientPath, "client/group.html"), response);
+  response.writeHeader(200, {"Content-Type":
+			     "text/html; charset=utf-8"});
 
-    while (i < length) {
-	char = 0;
-	from = "";
-	while (char != 10) {
-	    char = buffer.readUInt8(i++);
-	    if (char != 10)
-		from += String.fromCharCode(char);
-	}
-	
-	char = 0;
-	subject = "";
-	while (char != 10) {
-	    char = buffer.readUInt8(i++);
-	    if (char != 10)
-		subject += String.fromCharCode(char);
-	}
+  if (! naked)
+    writeFile(path.join(clientPath, "client/group.html"), response);
 
-	var time = buffer.readUInt32LE(i);
-	i += 8;
-
-	var article = buffer.readUInt32LE(i);
-	var rootArticle = article;
-	var comments = 0;
-	i += 4;
-	while (article) {
-	    var article = buffer.readUInt32LE(i);
-	    i += 4;
-	    comments++;
-	}
-	// Zero-terminated list of articles.
-	comments--;
-	
-	response.write("<div class=root>");
-	if (from.length > 0)
-	    response.write("<span class=from>" + from + "</span>: ");
-	response.write("<a href=\"/thread/" + group + "/" + rootArticle +
-		       "\">" +
-		       "<span class=subject>" +
-		       subject + "</span></a>", "binary");
-	if (comments > 0)
-	    response.write("<span class=comments>" + comments + " comments</span>");
+  while (i < length) {
+    char = 0;
+    from = "";
+    while (char != 10) {
+      char = buffer.readUInt8(i++);
+      if (char != 10)
+	from += String.fromCharCode(char);
     }
-    response.write("<script>decorateGroup(\"" + group + "\");</script>");
-    response.end();
-}
 
-function outputThread(url, response) {
-    var regs = url.match(/\/thread\/([^\/]+)\/([0-9]+)(\/naked)?/);
-    if (! regs)
-	return;
-    var group = regs[1];
-    var article = parseInt(regs[2]);
-    var naked = regs[3];
+    char = 0;
+    subject = "";
+    while (char != 10) {
+      char = buffer.readUInt8(i++);
+      if (char != 10)
+	subject += String.fromCharCode(char);
+    }
 
-    var warp = path.normalize(root + "/" + group.replace(/\./g, "/") + "/WARP");
-    var directory = path.normalize(spool + "/" + group.replace(/\./g, "/")
-				   + "/");
-
-    path.exists(warp, function(exists) {
-        if (! exists) {
-	    issue404(response);
-	    return;
-	}
-
-	fs.open(warp, "r", function(err, fd) {
-	    if (err) {
-		util.puts(err);
-		return;
-	    }
-
-	    // Find the start of each segment.
-	    var buffer = new Buffer(8);
-   	    fs.read(fd, buffer, 0, 8, 0, function(err, bytesRead) {
-		var numberOfRoots = buffer.readUInt32LE(0)
-		var lastArticle = buffer.readUInt32LE(4)
-		if (article > lastArticle) {
-		    issue404(response);
-		    return;
-		}
-		// Find the start of the root segment.
-   		fs.read(fd, buffer, 0, 4, 4 * (2 + article),
-			function(err, bytesRead) {
-			    var articleStart = buffer.readUInt32LE(0);
-			    buffer = new Buffer(1024);
-   			    fs.read(fd, buffer, 0, 1024,
-				    articleStart, function(err, bytesRead) {
-					writeThread(response, buffer,
-						    group, naked);
-				    });
-			});
-	    });
-	});
-    });
-}
-
-function writeThread(response, buffer, group, naked) {
-    var i = 0;
-    var length = buffer.length;
-    var char;
-    var from;
-    var groupPath = spool + group.replace(/\./g, "/") + "/";
-    
-    response.writeHeader(200, {"Content-Type":
-			       "text/html; charset=utf-8"});
-    // From
-    while (buffer.readUInt8(i++) != 10)
-	;
-	
-    // Subject
-    while (buffer.readUInt8(i++) != 10)
-	;
-
-    // Time
+    var time = buffer.readUInt32LE(i);
     i += 8;
-
-    var articles = new Array();
 
     var article = buffer.readUInt32LE(i);
     var rootArticle = article;
-    articles.push(article);
+    var comments = 0;
     i += 4;
     while (article) {
-	var article = buffer.readUInt32LE(i);
-	i += 4;
-	if (article != 0)
-	    articles.push(article);
+      var article = buffer.readUInt32LE(i);
+      i += 4;
+      comments++;
+    }
+    // Zero-terminated list of articles.
+    comments--;
+
+    response.write("<div class=root>");
+    if (from.length > 0)
+      response.write("<span class=from>" + from + "</span>: ");
+    response.write("<a href=\"/thread/" + group + "/" + rootArticle +
+		   "\">" +
+		   "<span class=subject>" +
+		   subject + "</span></a>", "binary");
+    if (comments > 0)
+      response.write("<span class=comments>" + comments + " comments</span>");
+  }
+  response.write("<script>decorateGroup(\"" + group + "\");</script>");
+  response.end();
+}
+
+function outputThread(url, response) {
+  var regs = url.match(/\/thread\/([^\/]+)\/([0-9]+)(\/naked)?/);
+  if (! regs)
+    return;
+  var group = regs[1];
+  var article = parseInt(regs[2]);
+  var naked = regs[3];
+
+  var warp = path.normalize(root + "/" + group.replace(/\./g, "/") + "/WARP");
+  var directory = path.normalize(spool + "/" + group.replace(/\./g, "/")
+				 + "/");
+
+  path.exists(warp, function(exists) {
+    if (! exists) {
+      issue404(response);
+      return;
     }
 
-    var artString = "";
-    articles.map(function(article) {
-	artString += " " + groupPath + article;
-    });
+    fs.open(warp, "r", function(err, fd) {
+      if (err) {
+	util.puts(err);
+	return;
+      }
 
-    var cacheFile = cache + group.replace(/\./g, "/") + "/" + rootArticle;
-    var child = exec(woof + " " + cacheFile + " " + artString,
-		     function (error, stdout, stderr) {
-			 if (error) {
-			     issue404(response);
-			     return;
-			 }
-			 response.writeHeader(200, {"Content-Type": "text/html; charset=utf-8"});
-			 if (! naked)
-			     writeFile(path.join(clientPath, "client/thread.html"), response);
-			 fs.readFile(cacheFile, "binary", function(err, file) {
-			     response.write(file, "binary");
-			     response.end();
-			 });
+      // Find the start of each segment.
+      var buffer = new Buffer(8);
+      fs.read(fd, buffer, 0, 8, 0, function(err, bytesRead) {
+	var numberOfRoots = buffer.readUInt32LE(0)
+	var lastArticle = buffer.readUInt32LE(4)
+	if (article > lastArticle) {
+	  issue404(response);
+	  return;
+	}
+	// Find the start of the root segment.
+   	fs.read(fd, buffer, 0, 4, 4 * (2 + article),
+		function(err, bytesRead) {
+		  var articleStart = buffer.readUInt32LE(0);
+		  buffer = new Buffer(1024);
+   		  fs.read(fd, buffer, 0, 1024,
+			  articleStart, function(err, bytesRead) {
+			    writeThread(response, buffer,
+					group, naked);
+			  });
+		});
+      });
+    });
+  });
+}
+
+function writeThread(response, buffer, group, naked) {
+  var i = 0;
+  var length = buffer.length;
+  var char;
+  var from;
+  var groupPath = spool + group.replace(/\./g, "/") + "/";
+
+  response.writeHeader(200, {"Content-Type":
+			     "text/html; charset=utf-8"});
+  // From
+  while (buffer.readUInt8(i++) != 10)
+    ;
+
+  // Subject
+  while (buffer.readUInt8(i++) != 10)
+    ;
+
+  // Time
+  i += 8;
+
+  var articles = new Array();
+
+  var article = buffer.readUInt32LE(i);
+  var rootArticle = article;
+  articles.push(article);
+  i += 4;
+  while (article) {
+    var article = buffer.readUInt32LE(i);
+    i += 4;
+    if (article != 0)
+      articles.push(article);
+  }
+
+  var artString = "";
+  articles.map(function(article) {
+    artString += " " + groupPath + article;
+  });
+
+  var cacheFile = cache + group.replace(/\./g, "/") + "/" + rootArticle;
+  var child = exec(woof + " " + cacheFile + " " + artString,
+		   function (error, stdout, stderr) {
+		     if (error) {
+		       issue404(response);
+		       return;
+		     }
+		     response.writeHeader(200, {"Content-Type": "text/html; charset=utf-8"});
+		     if (! naked)
+		       writeFile(path.join(clientPath, "client/thread.html"), response);
+		     fs.readFile(cacheFile, "binary", function(err, file) {
+		       response.write(file, "binary");
+		       response.end();
 		     });
+		   });
 }
 
 function writeFile(fpath, response) {
-    response.write(fs.readFileSync(fpath, "binary"), "binary");
+  response.write(fs.readFileSync(fpath, "binary"), "binary");
 }
 
 util.puts("Server Running on 8080");
